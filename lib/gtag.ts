2235a39ed610bb2b2eaeb1a866d685b828091668
Kite <ixkaito@gmail.com>
2021-05-27 08:07:03 +0900

    Fix bugs of gtag

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -2,9 +2,11 @@ export const GA_TRACKING_ID = ''
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL): void => {
-  window.gtag('config', GA_TRACKING_ID, {
-    page_path: url,
-  })
+  if (window && window.gtag) {
+    window.gtag('config', GA_TRACKING_ID, {
+      page_path: url,
+    })
+  }
 }
 
 type GTagEvent = {
