c2b505a3263d6033ecd9d2277d9a618d28b8a80f
Kite <ixkaito@gmail.com>
2021-05-27 08:32:01 +0900

    Remove .vscode

--- pages/about.tsx
+++ pages/about.tsx
@@ -9,8 +9,8 @@ export default function About(): JSX.Element {
         title="About"
       />
       <div className="max-w-xl mx-auto">
-        <h1 className={styles.title}>About</h1>
-        <p className="mt-4">Lorem ipsum dolor sit amet consectetur adipiscing elit risus lobortis, diam elementum metus suscipit inceptos vehicula convallis ridiculus luctus, est volutpat eleifend posuere aliquet accumsan a fusce. Ligula leo hendrerit non himenaeos mattis cum blandit senectus tempor velit, interdum pharetra eget elit pellentesque scelerisque class augue netus, rhoncus rutrum malesuada luctus adipiscing proin ullamcorper parturient inceptos. Class ut pretium mattis blandit velit augue conubia montes sagittis fringilla, ad placerat turpis platea fermentum tempor orci inceptos aenean, habitasse nascetur condimentum amet pharetra parturient ac dolor aliquet.</p>
+        <h1 className={styles.title}>About NextSSS</h1>
+        <p className="mt-4">NextSSS is a Next.js static site starter including full setup for TypeScript, Tailwind CSS, Google Analytics, Next SEO, etc.</p>
       </div>
     </Page>
   )
