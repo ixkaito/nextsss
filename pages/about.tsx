65a103b7b583e4bd1fe5c9c72c7dc701aafd42e5
Kite <ixkaito@gmail.com>
2021-06-20 17:42:00 +0900

    Add next/image example

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,5 @@
 import { NextSeo } from 'next-seo'
+import Image from 'next/image'
 import Layout from '../components/Layout'
 import styles from '../styles/About.module.scss'
 
@@ -8,6 +9,14 @@ const About: React.FC = () => {
       <NextSeo title="About" />
       <div className="max-w-xl mx-auto">
         <h1 className={styles.title}>About NextSSS</h1>
+        <figure className="mt-4">
+          <Image
+            src="/building.jpg"
+            width={3150}
+            height={2100}
+            alt="Building"
+          />
+        </figure>
         <p className="mt-4">
           NextSSS is a Next.js static site starter including full setup for
           TypeScript, Tailwind CSS, Google Analytics, Next SEO, etc.
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import { NextSeo } from 'next-seo'
+import Image from 'next/image'
 import Layout from '../components/Layout'
 import styles from '../styles/Home.module.scss'
+import building from '../public/building.jpg'
 
 const Home: React.FC = () => {
   return (
@@ -13,6 +15,9 @@ const Home: React.FC = () => {
         }}
       />
       <h1 className={`text-center uppercase ${styles.title}`}>Hello, World!</h1>
+      <figure className="mt-8">
+        <Image src={building} alt="Building" />
+      </figure>
     </Layout>
   )
 }
