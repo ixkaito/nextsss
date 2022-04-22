aab7279d6bfa9387ae5c47d295f931fa8d7884d2
Kite <ixkaito@gmail.com>
2022-04-22 21:08:14 +0900

    `mode: 'jit'` is not necessary from v3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@
  * @type {import('tailwindcss/tailwind-config').TailwindConfig }
  **/
 const tailwindConfig = {
-  mode: 'jit',
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
